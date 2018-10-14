2092667e5fbd9b622d4a7e1e9a84ea920179db49
Chad Schulz <chad.awesome@gmail.com>
2018-10-13 17:44:58 -0700

    Make SetColor anonymous

--- SetColor.cs
+++ SetColor.cs
@@ -14,7 +14,7 @@ namespace Colorator
     {
         [FunctionName("SetColor")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
